003454273d8c07cf2e896c7dfbf9c1f89c0ef8c0
fettoukh <medamine.fettoukh@gmail.com>
2021-10-12 22:12:50 +0200

    added 2 more nodes

--- taxi-ny-kedro/src/taxi_ny_kedro/pipelines/dataEng/nodes/preprocess.py
+++ taxi-ny-kedro/src/taxi_ny_kedro/pipelines/dataEng/nodes/preprocess.py
@@ -9,3 +9,29 @@ def limit_data_size(data ,limit_size):
         logger.warning("No limit size provided !")
         return data
     return data.head(limit_size)
+
+
+# def rename_columns(data ,old , new):
+#     print("node Started !!!")
+#     data.rename(columns={old : new})
+#     return data
+
+def rename_columns(data ):
+    print("node Started !!!")
+    # for columnName in data.columns:
+    #     # print(columnName)
+    #     print(columnName.capitalize())
+    #     data.rename(columns={columnName : columnName.capitalize()})
+
+    data.columns = [x.capitalize() for x in data.columns]
+
+    return data
+
+def clean_data(data, maxLat , maxLong):
+    print("node Started !!!")
+
+    data = data.drop(data[(data.pickup_latitude > maxLat) | (data.pickup_latitude < -maxLat) | (data.dropoff_latitude > maxLat) | (data.dropoff_latitude < -maxLat)].index)
+    data = data.drop(data[(data.pickup_longitude > maxLong) | (data.pickup_longitude < -maxLong) | (data.pickup_longitude > maxLong) | (data.pickup_longitude < -maxLong)].index)
+
+
+    return data
--- taxi-ny-kedro/src/taxi_ny_kedro/pipelines/dataEng/pipeline.py
+++ taxi-ny-kedro/src/taxi_ny_kedro/pipelines/dataEng/pipeline.py
@@ -1,5 +1,7 @@
 from kedro.pipeline import Pipeline,node
 from .nodes.preprocess import limit_data_size 
+from .nodes.preprocess import rename_columns 
+from .nodes.preprocess import clean_data
 
 def dataEng_pipeline():
     return Pipeline(
@@ -10,6 +12,21 @@ def dataEng_pipeline():
                 # input = ["yellow_tripData" , "parametrs"],    parameters["limit_size"]
                 outputs="yellow_tripDataLimited",
                 # name="limit_data_size"
-            )
+            ),
+             node(
+                func = rename_columns,
+                inputs = ["yellow_tripDataLimited"],
+                # input = ["yellow_tripData" , "parametrs"],    parameters["limit_size"]
+                outputs="yellow_tripDataRenamed",
+                # name="limit_data_size"
+            ),
+              node(
+                func = clean_data,
+                inputs = ["yellow_tripData", "params:maxLat", "params:maxLong"],
+                # input = ["yellow_tripData" , "parametrs"],    parameters["limit_size"]
+                outputs="yellow_tripDataCleaned",
+                # name="limit_data_size"
+            ),
+
         ]
     )
